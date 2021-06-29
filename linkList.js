80a13e60c38d952864e88bc79759d4e3d8fd551e
hurenren <hurenfa@gmail.com>
2021-06-29 23:56:54 +0800

    提交

---
+++ linkList.js
@@ -0,0 +1,96 @@
+// 节点
+class Node {
+  constructor(element, next) {
+    this.element = element; // 主体
+    this.next = next; // 关联体
+  }
+}
+
+/**
+ * @一
+ * @链表
+ * @description 链表结构类，并提供链表的操作方法
+ */
+class linkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;  // 长度，操作下标用
+  }
+
+  // 根据下标获取节点
+  _getNode(index) {
+    let node = this.head;
+    for (let i = 0; i < index; i++) {
+      node = node.next;
+    }
+    return node;
+  }
+
+  getNode(index) {
+    if (index >= this.size) return;
+    return this._getNode(index);
+  }
+
+  // 添加节点
+  add(index, element) {
+    if (arguments.length === 1) { // 如果只传一个参数，表明只传了节点
+      element = index;
+      index = this.size;
+    }
+    if (index > this.size) return;  // 如果索引大于长度，无法操作
+    if (index === 0) {  // 0代表要替换表头，原表头后移
+      const oldHead = this.head;
+      this.head = new Node(element, oldHead) // 老表头放到新节点关联体
+    } else {
+      const prevNode = this._getNode(index - 1); // 找到前一个节点
+      prevNode.next = new Node(element, prevNode.next) // 上一个节点的新关联体就是新节点，新节点的关联体就是上一个节点的老关联体关联体
+    }
+    this.size++;
+  }
+
+  // 删除节点
+  remove(index) {
+    if (index >= this.size) return;
+    let removeNode; // 删除体
+    if (index === 0) {  // 删除头就是把头的关联体变为新头
+      removeNode = this.head;
+      if (removeNode === null) return;
+      this.head = removeNode.next;
+    } else {
+      const prevNode = this._getNode(index - 1); // 找到前一个节点
+      if (!prevNode) return;
+      removeNode = prevNode.next;
+      prevNode.next = removeNode.next; // 前一个节点的关联体就是删除体的关联体
+    }
+    this.size--;
+    return removeNode;
+  }
+
+  update(index, element) {
+    if (index >= this.size) return;
+    const node = this._getNode(index);
+    node.element = element;
+    return node;
+  }
+
+  // 反转-递归
+  reverse() {
+    function reverse(head) {
+      if (head === null || head.next === null) return head;
+      const newHead = reverse(head.next); // 新头变为下一个
+      head.next.next = head; // 让下一个人的next 指向老的头
+      head.next = null; // 老的头下一个指向是的null
+      return newHead; // newHead和head如果有相同的element，指向的是同一个地址
+    }
+    return reverse(this.head);
+  }
+}
+
+const link = new linkedList;
+link.add(0);
+link.add(1);
+link.add(2);
+link.add(3);
+// link.getNode(6);
+
+console.log(link.reverse());
